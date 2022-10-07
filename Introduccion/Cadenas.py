154edb43e0463d5ce0c7eab0c51b485f33352261
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-10-07 00:34:49 -0300

    Cadenas de Texto

---
+++ Introduccion/Cadenas.py
@@ -0,0 +1,18 @@
+#Cadenas de texto
+
+texto="Programación de MediaNoche"
+
+print("TEXTO COMPLETO")
+print(texto)
+
+print(" ")
+print("IMPRIME LA PRIMERA LETRA")
+print(texto[0])
+
+print(" ")
+print("IMPRIME LA PALABRA DEL MEDIO")
+print(texto[13:15])
+
+print(" ")
+print("IMPRIME LA ULTIMA PALABRA")
+print(texto[-10:])
