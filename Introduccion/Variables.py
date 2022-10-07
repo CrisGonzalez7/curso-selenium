4b7716bf1962ef70c9ea678c794235ef6896eec0
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-10-06 23:40:20 -0300

    Variables en Python

---
+++ Introduccion/Variables.py
@@ -0,0 +1,27 @@
+# Introduccion a las variables
+
+# Numeros
+a=10
+b=20
+
+suma=a+b
+resta=a-b
+multiplicacion=a*b
+division=a/b
+
+print("la suma es: "+ str(suma))
+print("la resta es: "+ str(resta))
+print("la multiplicacion es: "+ str(multiplicacion))
+print("la division es: "+ str(division))
+
+# Strings
+print(" ")
+nombre="Cris"
+apellido="Gonzalez"
+print("Mi nombre es: "+nombre +" "+apellido)
+
+#Tipos
+print(" ")
+print("El tipo de variable de A es: "+str(type(a)))
+print("El tipo de variable de Nombre es: "+str(type(nombre)))
+
