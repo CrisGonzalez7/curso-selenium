d90bf22ba29ef9c6a1cf68dd2bcdfd9f620ce574
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-10-07 00:20:15 -0300

    Conversiones en Python + Correciones en Variables

---
+++ Introduccion/Conversiones.py
@@ -0,0 +1,34 @@
+# Conversiones
+
+#Variables
+a=20
+b=30
+resultado=a+b
+nombre="Cris"
+
+#Tipo de Variables
+print("TIPO DE VARIABLES")
+print("El tipo de variable de A es: "+str(type(a)))
+print("El tipo de variable de B es: "+str(type(b)))
+print("El tipo de variable de Nombre es: "+str(type(nombre)))
+
+#Suma de A+B
+print(" ")
+print("SUMA DE A+B")
+print("La suma de a+b es: "+str(resultado))
+
+#Cambio de Tipo de Variable
+a=str(a)
+b=float(b)
+
+print(" ")
+print("CAMBIO DE TIPO DE VARIABLE")
+print("El tipo de variable de A es: "+str(type(a)))
+print("El tipo de variable de B es: "+str(type(b)))
+
+#Numeros a Strings
+b=str(b)
+resultado=a+b
+print(" ")
+print("NUMEROS A STRINGS")
+print("La suma de a+b como Strings es: "+str(resultado))
--- Introduccion/Variables.py
+++ Introduccion/Variables.py
@@ -1,6 +1,6 @@
 # Introduccion a las variables
 
-# Numeros
+# Variables
 a=10
 b=20
 
@@ -9,19 +9,24 @@ resta=a-b
 multiplicacion=a*b
 division=a/b
 
+#Numeros
+print("NUMEROS")
 print("la suma es: "+ str(suma))
 print("la resta es: "+ str(resta))
 print("la multiplicacion es: "+ str(multiplicacion))
 print("la division es: "+ str(division))
 
 # Strings
-print(" ")
 nombre="Cris"
 apellido="Gonzalez"
+
+print(" ")
+print("STRINGS")
 print("Mi nombre es: "+nombre +" "+apellido)
 
 #Tipos
 print(" ")
+print("TIPOS DE VARIABLES")
 print("El tipo de variable de A es: "+str(type(a)))
 print("El tipo de variable de Nombre es: "+str(type(nombre)))
 
