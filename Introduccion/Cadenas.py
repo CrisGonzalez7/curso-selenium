ad539e0b3ca1e12d94f14189e2dff07d67f6d4ea
Veronica Gonzalez <veronicagonzalez@serviciodigital.cl>
2022-10-07 00:46:49 -0300

    Cadenas y Arreglos

--- Introduccion/Cadenas.py
+++ Introduccion/Cadenas.py
@@ -16,3 +16,28 @@ print(texto[13:15])
 print(" ")
 print("IMPRIME LA ULTIMA PALABRA")
 print(texto[-10:])
+
+#Funciones con Cadenas
+
+print(" ")
+print("IMPRIME LA PALABRA EN MAYUSCULAS")
+print(texto[13:15].upper())
+
+print(" ")
+print("TEXTO COMPLETO EN MINUSCULAS")
+print(texto.lower())
+
+print(" ")
+print("IMPRIME LA PALABRA CON INICIAL MAYUSCULA")
+print(texto[13:15].capitalize())
+
+#Elementos separados por coma
+cadena="html,css,sass,less,javascript,angular"
+
+print(" ")
+print("IMPRIME CADENA COMPLETA")
+print(cadena)
+
+print(" ")
+print("CADENA COMO ARREGLO")
+print(cadena.split(','))
